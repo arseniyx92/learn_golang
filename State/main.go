14f47e34c9be8efb26de80e31cbb42f3b4ed0aab
Arseniy Khlytchiev <arseniykhlytchiev@gmail.com>
2020-06-13 15:08:43 +0300

    description added

--- State/main.go
+++ State/main.go
@@ -7,11 +7,15 @@ import (
 
 func main(){
 	http.HandleFunc("/", foo)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	//to fivicon not loaded, coz it's not allowed to load on the page - obvious
+	//https://github.com/GoesToEleven/golang-web-dev/blob/master/027_passing-data/05_form-file/01_read/main.go
 	http.ListenAndServe(":8080", nil)
 }
 
 func foo(w http.ResponseWriter, req *http.Request){
 	v := req.FormValue("q")
+	// it's (f, h, err := req.FormFile) i we wanna file
 	io.WriteString(w, "U r searching for " + v)
 }
 
@@ -19,3 +23,5 @@ func foo(w http.ResponseWriter, req *http.Request){
 //it works by GET and POST
 //only one thing is in URL, it has always 'q' by default
 //in POST it's 'name' where something like 'q' contains
+
+//https://github.com/GoesToEleven/golang-web-dev/tree/master/027_passing-data
