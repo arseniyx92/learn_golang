cb936b816e948f5bd760d1e4a86abfad11acee13
Arseniy Khlytchiev <arseniykhlytchiev@gmail.com>
2020-06-13 11:45:02 +0300

    Passing data in url - part 1

--- State/main.go
+++ State/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"io"
+	"net/http"
+)
+
+func main(){
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":8080", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request){
+	v := req.FormValue("q")
+	io.WriteString(w, "U r searching for " + v)
+}
+
+//localhost:8080/?q=dog
